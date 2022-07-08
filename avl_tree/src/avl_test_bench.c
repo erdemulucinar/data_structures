3ba9a8aa1f8806aaae71020160d42155a9ab0a49
uluerdem <erdemulucinar@outlook.com>
2022-07-08 23:52:24 +0300

    Test bench definitions changed
    Since integer related test bench functions are at a new location, related changes are made.

--- avl_tree/src/avl_test_bench.c
+++ avl_tree/src/avl_test_bench.c
@@ -1,59 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "test_bench_defs.h"
 #include "avl_tree.h"
-
-//AVL key functions for integers
-void* copyInt(void *a){
-    int *x,*y;
-
-    x = (int*) a;
-    y = malloc(sizeof(int));
-    *y = *x;
-    
-    return (void*) y;
-}
-
-void delInt (void *a){
-    int *x;
-    
-    x = (int*) a;
-    free(x);
-}
-
-int compInt(void *a, void *b){
-    int *x,*y;
-    
-    x = (int*) a;
-    y = (int*) b;
-    
-    return *x - *y;
-}
-
-void printInt(void *a){
-    int *x;
-    
-    x = (int*) a;
-    printf("%d",*x);
-}
-
-//Test bench helpers
-int* randList(int len){
-    int *list, i, j, temp;
-    
-    list = malloc(len*sizeof(int));
-    for(i = 0; i < len; i++)
-        list[i] = i;
-    
-    for(i = 0; i < len; i++){
-        j = rand() % (len-i) + i;
-        temp = list[i];
-        list[i] = list[j];
-        list[j] = temp;
-    }
-    
-    return list;
-}
 
 AVLTree* insertionTest(int len, int *isSortedAcc, int *maxDevAcc, int *treeLenAcc){
     AVLTree *tree;
